c9f91d0f9fba6cd89bae3aa7366b679e657ebc91
Niklas Pettersson <nikpe890@olympen-15.edu.isy.liu.se>
2011-05-12 21:43:08 +0200

    lololol

--- lab1/lab1-2.c
+++ lab1/lab1-2.c
@@ -1,4 +1,4 @@
-
+#include "FTGL/ftgl.h"
 #include <GL/glut.h>
 #include <math.h>
 
--- main/scenes/space.c
+++ main/scenes/space.c
@@ -17,8 +17,8 @@ void space_init()
 {
   // Place one-time initialization code here
 spacetextureId = loadTexture("../textures/Space.jpg");
-megatextureId[0] = loadTexture("../textures/megamanmask.jpg");
-megatextureId[1] = loadTexture("../textures/megamantest.jpg");
+megatextureId[0] = loadTexture("../textures/3d-star-mask.jpg");
+megatextureId[1] = loadTexture("../textures/3d-star2.jpg");
 }
 
 void space_load() {
@@ -82,7 +82,7 @@ void space_disp()
   zdepth = 590.0;
   xwidth = 600;
   yheight = 400.0;
-  starDepth = zdepth-40;
+  starDepth = zdepth-460;
 
 
   //do the wall 
@@ -116,29 +116,62 @@ glColor3f(0.9,0.9,0.9);
   glVertex3f(xwidth,-yheight,-zdepth+1);
   glEnd();
 
+//the moving stuff :D
+glPushMatrix();
 
-//make megaman :D
-glEnable(GL_TEXTURE_2D);
-glEnable(GL_BLEND);  
-glDisable(GL_DEPTH_TEST);
-glBlendFunc(GL_DST_COLOR,GL_ZERO);
+glTranslatef(-getElapsedTime()*50,0,0);
+/*
+float megaDepth = zdepth;
+float megaheight = yheight;
+float megaWidth = xwidth;
 float textureStart =0.0;
 float textureFinish =1.0;
 float tS = textureStart;
 float tF = textureFinish;
-float xXx = 0.5;
-//glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_REPEAT);
+float xXx = 0.44;
+
+glEnable(GL_TEXTURE_2D);
+glBindTexture(GL_TEXTURE_2D, megatextureId[0]);
+
+glBegin(GL_QUADS);				// Start Drawing A Textured Quad
+glTexCoord2f(tF, tF);
+glVertex3f(megaWidth, -megaheight-160.0f,-megaDepth);	// Bottom Left
+glTexCoord2f(tS, tF);
+glVertex3f(megaWidth+1000.0f,-megaheight-160.0f,-megaDepth);	// Bottom Right
+glTexCoord2f(tS, tS);
+glVertex3f(megaWidth+1000.0f,-megaheight,-megaDepth);	// Top Right
+ glTexCoord2f(tF, tS);
+glVertex3f(megaWidth,-megaheight,-megaDepth);	// Top Left
+glEnd();
+*/
+//************************STAR START************************************//
+
+glDisable(GL_DEPTH_TEST);
+glEnable(GL_TEXTURE_2D);
+glEnable(GL_BLEND);  
+glBlendFunc(GL_DST_COLOR,GL_ZERO);
+float megaDepth = zdepth;
+float megaheight = yheight;
+float megaWidth = xwidth;
+float textureStart =0.0;
+float textureFinish =1.0;
+float tS = textureStart;
+float tF = textureFinish;
+float xXx = 0.44;
+glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_CLAMP);
 glBindTexture(GL_TEXTURE_2D, megatextureId[0]);		// Select The First Mask Texture
-glBegin(GL_QUADS);					// Start Drawing A Textured Quad
+glBegin(GL_QUADS);				// Start Drawing A Textured Quad
 glTexCoord2f(xXx+tF, tF);
-glVertex3f(0.0f, 0.0f,-2.0f);	// Bottom Left
+glVertex3f(megaWidth, -megaheight,-megaDepth);	// Bottom Left
 glTexCoord2f(xXx+tS, tF);
-glVertex3f( 1.0f, 0.0f,-2.0f);	// Bottom Right
+glVertex3f(megaWidth+160.0f, -megaheight,-megaDepth);	// Bottom Right
 glTexCoord2f(xXx+tS+0.5, tS);
-glVertex3f( 1.0f,  1.0f,-2.0f);	// Top Right
-glTexCoord2f(xXx+tF+0.5, tS);
-glVertex3f(0.0f,  1.0f,-2.0f);	// Top Left
-glEnd();						// Done Drawing The Quad
+glVertex3f(megaWidth+160.0f,  -megaheight+160.0f,-megaDepth);	// Top Right
+ glTexCoord2f(xXx+tF+0.5, tS);
+glVertex3f(megaWidth,  -megaheight+160.0f,-megaDepth);	// Top Left
+glEnd();
+
+
 
 /*Again we enable blending and select our texture for scene 1. We map this texture on top of it's mask. */
 		
@@ -146,43 +179,23 @@ glBlendFunc(GL_ONE, GL_ONE);			// Copy Image 1 Color To The Screen
 glBindTexture(GL_TEXTURE_2D, megatextureId[1]);	// Select The First Image Texture
 glBegin(GL_QUADS);				// Start Drawing A Textured Quad
 glTexCoord2f(xXx+tF, tF);
-glVertex3f(0.0f, 0.0f,-2.0f);	// Bottom Left
+glVertex3f(megaWidth, -megaheight,-megaDepth);	// Bottom Left
 glTexCoord2f(xXx+tS, tF);
-glVertex3f( 1.0f, 0.0f,-2.0f);	// Bottom Right
+glVertex3f(megaWidth+160.0f, -megaheight,-megaDepth);	// Bottom Right
 glTexCoord2f(xXx+tS+0.5, tS);
-glVertex3f( 1.0f,  1.0f,-2.0f);	// Top Right
+glVertex3f(megaWidth+160.0f,  -megaheight+160.0f,-megaDepth);	// Top Right
  glTexCoord2f(xXx+tF+0.5, tS);
-glVertex3f(0.0f,  1.0f,-2.0f);	// Top Left
-	
-//glBegin(GL_QUADS);
-glEnd();							// Done Drawing The Quad
+glVertex3f(megaWidth,  -megaheight+160.0f,-megaDepth);	// Top Left
+glEnd();
 
 glDisable(GL_TEXTURE_2D);
 glEnable(GL_DEPTH_TEST); 
 glDisable(GL_BLEND);
 
+//************************STAR END************************************//
 
-
-
-
-
-
-
-
-
-//the moving stuff :D
-glPushMatrix();
-
-glTranslatef(-getElapsedTime()*50,0,0);
-//glRotatef(45,0,0,1);
-
-  starsize = 15;
-  starOffsetX = 0;
-  starOffsetY = 0;
-//Layer 1 :D
 
 makeEmStars();
-
 
 //Layer 2 :D
 //xwidth = 380;
@@ -198,8 +211,6 @@ makeEmStars();
 //makeEmStars(xwidth, yheight, zdepth);
 
 glPopMatrix();
-
-
   // Swap front- and backbuffers
   glutSwapBuffers();
 }
@@ -207,22 +218,70 @@ glPopMatrix();
 void idle()
 {
   // This function is called whenever the computer is idle
-
   // As soon as the machine is idle, ask GLUT to trigger rendering of a new
   // frame
   glutPostRedisplay();
 }
 
+
 void makeEmStars() 
 {
 drawAStar();
+starOffsetX = 1500;
+starOffsetY = 650;
 
-starOffsetX = 100;
-starOffsetY = 200;
 drawAStar();
-
 starOffsetX = -starOffsetX/2;
 starOffsetY = -starOffsetY/2;
+
+drawAStar();
+starOffsetX = -starOffsetX/2;
+starOffsetY = -starOffsetY/2;
+
+drawAStar();
+starOffsetX = -starOffsetX/3;
+starOffsetY = -starOffsetY/3;
+
+drawAStar();
+starOffsetX = -starOffsetX/4;
+starOffsetY = -starOffsetY*5;
+
+drawAStar();
+starOffsetX = -starOffsetX*72;
+starOffsetY = -starOffsetY/4;
+
+drawAStar();
+starOffsetX = -starOffsetX/2;
+starOffsetY = -starOffsetY/2;
+
+drawAStar();
+starOffsetX = -starOffsetX*2;
+starOffsetY = -starOffsetY*7;
+
+drawAStar();
+starOffsetX = -starOffsetX/2;
+starOffsetY = -starOffsetY/2;
+
+drawAStar();
+starOffsetX = -starOffsetX-20;
+starOffsetY = -starOffsetY-50;
+
+drawAStar();
+starOffsetX = -starOffsetX/2;
+starOffsetY = -starOffsetY/2;
+
+drawAStar();
+starOffsetX = -starOffsetX/3;
+starOffsetY = -starOffsetY/2.5;
+
+drawAStar();
+starOffsetX = -starOffsetX*4;
+starOffsetY = -starOffsetY*3.4;
+
+drawAStar();
+starOffsetX = -starOffsetX-25;
+starOffsetY = -starOffsetY+18;
+
 drawAStar();
 }
 
@@ -238,7 +297,7 @@ void drawAStar() {
   glEnd();
 
 */
-
+/*
 glBegin(GL_POLYGON);
 glColor3f(0.9,0.9,0.9);
   glVertex3f(0-starOffsetX,starsize+starOffsetY,-starDepth);
@@ -249,6 +308,31 @@ glColor3f(0.9,0.9,0.9);
   glVertex3f(0-starOffsetX, -starsize/4+starOffsetY,-starDepth);
 glEnd();
 
+//Layer2
+glBegin(GL_QUADS);
+glColor3f(0.9,0.9,0.9);
+  glVertex3f(0-starOffsetX,starsize+starOffsetY,-starDepth);
+  glVertex3f(-starsize/5-starOffsetX,starsize/5+starOffsetY,-starDepth);
+  glVertex3f(-starsize-starOffsetX,starOffsetY,-starDepth);
+  glVertex3f(-starsize/4-starOffsetX,-starsize/4+starOffsetY,-starDepth);
+glEnd();
+
+
+glBegin(GL_POLYGON);
+  glVertex3f(-starsize/3-starOffsetX,-starsize+starOffsetY,-starDepth);
+  glVertex3f(0-starOffsetX, -starsize/4+starOffsetY,-starDepth);
+  glVertex3f(
+
+glBegin(GL_POLYGON);
+glColor3f(0.9,0.9,0.9);
+  glVertex3f(0-starOffsetX,starsize+starOffsetY,-starDepth);
+  glVertex3f(-starsize/5-starOffsetX,starsize/5+starOffsetY,-starDepth);
+  glVertex3f(-starsize-starOffsetX,starOffsetY,-starDepth);
+  glVertex3f(-starsize/4-starOffsetX,-starsize/4+starOffsetY,-starDepth);
+  glVertex3f(-starsize/3-starOffsetX,-starsize+starOffsetY,-starDepth);
+  glVertex3f(0-starOffsetX, -starsize/4+starOffsetY,-starDepth);
+glEnd();
+*/
 }
 /*
 int main(int argc, char **argv)
